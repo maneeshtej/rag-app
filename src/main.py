3d3589f17a3d8d38f7a12dbbf6982e5aac17b625
apple <apple@apples-MacBook-Air.local>
2025-12-21 23:47:57 +0530

    entry point(cli)

---
+++ src/main.py
@@ -0,0 +1,80 @@
+from pipelines.ingestion_pipeline import IngestionPipeline
+from pipeline import MainPipeline
+from langchain_community.document_loaders import TextLoader
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from services.user_service import create_user, user_login
+from bootstrap.bootstrap import create_app
+
+
+def main():
+    print("_" * 100)
+    print("Simple Naive RAG CLI")
+    print("Commands:")
+    print("  create        -> create a user")
+    print("  login         -> login as a user")
+    print("  query         -> ask a question")
+    print("  add           -> ingest a text file")
+    print("  exit          -> quit")
+    print("_" * 100)
+
+    app = create_app()
+
+    current_user = None  
+
+    while True:
+        cmd = input(">> ").strip()
+
+        if cmd.lower() in {"exit", "quit"}:
+            break
+
+        elif cmd.lower() == "create":
+            username = input("username: ").strip()
+            role = input("role (admin/user/etc): ").strip()
+            level = int(input("access level (1=highest): ").strip())
+
+            current_user = create_user(
+                username=username,
+                role=role,
+                access_level=level,
+            )
+
+            print("User created & set as current:")
+            print(current_user)
+
+        elif cmd.lower() == "login":
+            username = input("username: ").strip()
+            user = user_login(username)
+            if user == None:
+                print(f"User {username} does not exist.")
+                continue
+
+            current_user = user
+
+        elif cmd.lower() == "query":
+            query = input("query: ")
+            results = app.retrieve(query, current_user)
+
+            print(results)
+
+        elif cmd.lower() == "add":
+            filepath = input("filepath: ")
+
+            if current_user is None:
+                print("No user set. Run `create` first.\n")
+                continue
+
+            loader = TextLoader(file_path=filepath)
+
+            chunks = app.ingest(
+                loader=loader,
+                user=current_user
+            )
+
+            print(chunks)
+
+        else:
+            print("Try again")
+
+
+if __name__ == "__main__":
+    main()
