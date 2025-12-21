96b7033213d27fd936df2b3e723ed818361a2294
apple <apple@apples-MacBook-Air.local>
2025-12-21 23:47:37 +0530

    moved bootstrapping from main.py to src/bootstrap

---
+++ src/bootstrap/bootstrap.py
@@ -0,0 +1,45 @@
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from database.vectorstore import create_vectorstore, create_embedder
+from database.db import get_connection
+from database.pg_vectorstore import PGVectorStore
+from database.pg_retrieval import PGRetriever
+from pipeline import MainPipeline
+from pipelines.ingestion_pipeline import IngestionPipeline
+from pipelines.retrieval_pipeline import RetrievalPipeline
+
+def create_ingestion_app():
+    splitter = RecursiveCharacterTextSplitter(
+        separators=["\n\n", "\n", ". ", " ", ""],
+        chunk_size=500,
+        chunk_overlap=50,
+    )
+    embedder = create_embedder()
+    conn = get_connection()
+    vectorstore = PGVectorStore(embedder=embedder, conn=conn)
+
+    ingestion = IngestionPipeline(splitter=splitter, vectorstore=vectorstore)
+  
+
+    return ingestion
+
+def create_retrival_app(k:int = 5):
+    embedder = create_embedder()
+    conn = get_connection()
+    vectorstore = PGVectorStore(embedder=embedder, conn=conn)
+
+    retriever = PGRetriever(vectorstore=vectorstore, embedder=embedder)
+
+    return(
+        RetrievalPipeline(retriever=retriever)
+    )
+
+
+
+def create_app():
+    app = MainPipeline(
+        ingestion=create_ingestion_app(),
+        retriever=create_retrival_app()
+        )
+
+    return app
+
