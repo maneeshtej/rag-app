d4423ad0ee6dc4e4c0057a07e3401b0c2f676014
apple <apple@apples-MacBook-Air.local>
2025-12-21 23:50:37 +0530

    added user auth methods

---
+++ src/services/user_service.py
@@ -0,0 +1,53 @@
+import uuid
+from database.db import get_connection
+from models.user import User
+
+def create_user(
+        username: str,
+        role: str,
+        access_level: int
+):
+    user_id = uuid.uuid4()
+
+    query = """
+    INSERT INTO users(id, username, role, access_level)
+    values(%s, %s, %s, %s)
+    RETURNING id, username, role, access_level;
+    """
+
+    with get_connection() as conn:
+        with conn.cursor() as cur:
+            cur.execute(query, (str(user_id), username, role, access_level))
+            row = cur.fetchone()
+            conn.commit()
+
+    return User(
+        id=row[0],
+        username=row[1],
+        role=row[2],
+        access_level=row[3]
+    )
+
+def user_login(username: str):
+    
+    
+    query = """
+    SELECT id, username, role, access_level FROM users
+    WHERE username = %s
+    """
+
+    with get_connection() as conn:
+        with conn.cursor() as cur:
+            cur.execute(query, (username, ))
+            row = cur.fetchone()
+            conn.commit()
+
+        if row == None:
+            return None
+        
+        return User(
+        id=row[0],
+        username=row[1],
+        role=row[2],
+        access_level=row[3]
+        )
